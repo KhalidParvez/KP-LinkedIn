36f69ae389e999520045e086d7ec5723b764f877
parvez.khalid <parvez.khalid@INBL01LAP1166>
2021-09-18 00:45:51 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KP-LinkedIn'`, () => {
+  it(`should have as title 'myApp1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KP-LinkedIn');
+    expect(app.title).toEqual('myApp1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('KP-LinkedIn app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myApp1 app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonDataEntryComponent } from './person-data-entry/person-data-entry.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PersonDataDisplayComponent } from './person-data-display/person-data-display.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
+import { StudentListingComponent } from './student-listing/student-listing.component';
+import { StudentServiceService } from './student-service.service';
+import {HttpClientModule} from '@angular/common/http';
+import { ProperNamePipe } from './proper-name.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
+import { DelayRenderingDirective } from './delay-rendering.directive';
+import { MinilinkedinComponent } from './minilinkedin/minilinkedin.component';
+import { MinilinkedinListComponent } from './minilinkedin-list/minilinkedin-list.component'
+import { LinkedinServiceService } from './linkedin-service.service';
+import { LinkedinProfileComponent } from './linkedin-profile/linkedin-profile.component';
+import { ForbiddenNameValidatorDirective } from './forbidden-name-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonDataEntryComponent,
+    PersonDataDisplayComponent,
+    StudentEntryComponent,
+    StudentListingComponent,
+    ProperNamePipe,
+    HighlightDirective,
+    UnlessDirective,
+    DelayRenderingDirective,
+    MinilinkedinComponent,
+    MinilinkedinListComponent,
+    LinkedinProfileComponent,
+    ForbiddenNameValidatorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    StudentServiceService,
+    LinkedinServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
